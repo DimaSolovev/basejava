b5f40da17a5c3bab4352dbefd168fa777debbc7e
Dmitrii Solovev <soloveid87@mail.ru>
2021-02-17 18:24:32 +1000

    HW3

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -4,37 +4,62 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
 
-public class SortedArrayStorage extends AbstractArrayStorage{
+public class SortedArrayStorage extends AbstractArrayStorage {
+
     @Override
     protected int getIndex(String uuid) {
         Resume searchKey = new Resume();
         searchKey.setUuid(uuid);
-
-        return Arrays.binarySearch(storage,0,size,searchKey);
+        return Arrays.binarySearch(storage, 0, size, searchKey);
     }
 
     @Override
     public void update(Resume resume) {
-
+        int index = getIndex(resume.getUuid());
+        if (index < 0) {
+            System.out.println("ERROR, storage doesn't contain resume " + resume.getUuid());
+        } else {
+            storage[index] = resume;
+        }
     }
 
     @Override
     public void save(Resume resume) {
-
+        int index = getIndex(resume.getUuid());
+        if (size == storage.length) {
+            System.out.println("ERROR, storage is full");
+            return;
+        }
+        if (index >= 0) {
+            System.out.println("ERROR, storage already contains resume " + resume.getUuid());
+        } else {
+            storage[-(index + 1)] = resume;
+            size++;
+        }
     }
 
     @Override
     public void delete(String uuid) {
-
+        int index = getIndex(uuid);
+        if (index < 0) {
+            System.out.println("ERROR, storage doesn't contain resume  " + uuid);
+        } else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        }
     }
 
     @Override
     public void clear() {
-
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     @Override
     public Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[size];
+        if (size >= 0) System.arraycopy(storage, 0, resumes, 0, size);
+        return resumes;
     }
 }
