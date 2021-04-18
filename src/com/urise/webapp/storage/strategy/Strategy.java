e1711bfddf75d3b7d4c7bc5a2fb1bf98aa747322
Dmitrii Solovev <soloveid87@mail.ru>
2021-04-18 23:36:42 +1000

    ex

--- src/com/urise/webapp/storage/strategy/Strategy.java
+++ src/com/urise/webapp/storage/strategy/Strategy.java
@@ -2,13 +2,14 @@ package com.urise.webapp.storage.strategy;
 
 import com.urise.webapp.model.Resume;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public interface Strategy {
 
-    void doWrite(Resume resume, OutputStream os);
+    void doWrite(Resume resume, OutputStream os) throws IOException;
 
-    Resume doRead(InputStream is);
+    Resume doRead(InputStream is) throws IOException;
 
 }
--- test/com/urise/webapp/storage/ObjectStreamStorageTest.java
+++ test/com/urise/webapp/storage/ObjectStreamStorageTest.java
@@ -2,11 +2,8 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.storage.strategy.ObjectStreamStrategy;
 
-public class ObjectStreamStorageTest extends AbstractStorageTest{
+public class ObjectStreamStorageTest extends AbstractStorageTest {
     public ObjectStreamStorageTest() {
-        super(new FileStorage(STORAGE_DIR,new ObjectStreamStrategy()));
+        super(new FileStorage(STORAGE_DIR, new ObjectStreamStrategy()));
     }
-//    public ObjectStreamStorageTest() {
-//        super(new ObjectStreamStorage(STORAGE_DIR));
-//    }
 }
