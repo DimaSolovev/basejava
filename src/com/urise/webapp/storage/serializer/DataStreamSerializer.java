1fd6fe4908afa4b2a23bbcfcebb903254e04a33d
Dmitrii Solovev <soloveid87@mail.ru>
2021-05-07 10:50:08 +1000

    ref

--- src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
@@ -25,24 +25,19 @@ public class DataStreamSerializer implements StreamSerializer {
 
             Map<SectionType, Section> sections = r.getSections();
 
-//            List<SectionType> types = new ArrayList<>(sections.keySet());
-//            dos.writeInt(types.size());
-//            for (SectionType type : types) {
-//                dos.writeUTF(type.name());
-//            }
-
             for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
-                switch (entry.getKey().name()) {
+                String sectionName = entry.getKey().name();
+                switch (sectionName) {
                     case ("OBJECTIVE"):
                     case ("PERSONAL"):
-                        dos.writeUTF(entry.getKey().name());
-                        dos.writeUTF(String.valueOf(entry.getValue()));
+                        dos.writeUTF(sectionName);
+                        dos.writeUTF(String.valueOf(entry.getValue().getClass()));
                         break;
                     case ("ACHIEVEMENT"):
                     case ("QUALIFICATIONS"):
                         ListSection listSection = (ListSection) entry.getValue();
                         List<String> list = listSection.getItems();
-                        dos.writeUTF(entry.getKey().name());
+                        dos.writeUTF(sectionName);
                         dos.writeInt(list.size());
                         for (String s : list) {
                             dos.writeUTF(s);
@@ -50,13 +45,15 @@ public class DataStreamSerializer implements StreamSerializer {
                         break;
                     case ("EXPERIENCE"):
                     case ("EDUCATION"):
-                        dos.writeUTF(entry.getKey().name());
+                        dos.writeUTF(sectionName);
                         OrganizationSection organizationSection = (OrganizationSection) entry.getValue();//организация
                         List<Organization> listOrg = organizationSection.getOrganizations();//список организаций
                         dos.writeInt(listOrg.size());//размер списка организаций
+                        Link orgLink;
                         for (Organization org : listOrg) { //проходим по списку организаций
-                            dos.writeUTF(org.getHomePage().getName());
-                            dos.writeUTF(org.getHomePage().getUrl());
+                            orgLink=org.getHomePage();
+                            dos.writeUTF(orgLink.getName());
+                            dos.writeUTF(orgLink.getUrl());
                             List<Organization.Position> listPos = org.getPositions();//получаем список позиций в каждой организации
                             dos.writeInt(listPos.size());//размер списка позиций
                             for (Organization.Position op : listPos) {//проходим по списку позиций
@@ -71,7 +68,6 @@ public class DataStreamSerializer implements StreamSerializer {
                         break;
                 }
             }
-            // TODO implements sections
         }
     }
 
@@ -117,9 +113,7 @@ public class DataStreamSerializer implements StreamSerializer {
                                 LocalDate startDate = LocalDate.of(dis.readInt(), dis.readInt(), 1);
                                 LocalDate endDate = LocalDate.of(dis.readInt(), dis.readInt(), 1);
                                 String title = dis.readUTF();
-                                //System.out.println(title);
                                 String description = dis.readUTF();
-                                //System.out.println(description);
                                 positions.add(new Organization.Position(startDate, endDate, title, description));
                             }
                             organizations.add(new Organization(link, positions));
@@ -128,7 +122,6 @@ public class DataStreamSerializer implements StreamSerializer {
                         break;
                 }
             }
-            // TODO implements sections
             return resume;
         }
     }
