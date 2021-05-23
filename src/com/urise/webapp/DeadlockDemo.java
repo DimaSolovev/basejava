d8389997611ea860e02f1bc58b06c14f0539297f
Dmitrii Solovev <soloveid87@mail.ru>
2021-05-23 18:00:05 +1000

    trhread

--- src/com/urise/webapp/DeadlockDemo.java
+++ src/com/urise/webapp/DeadlockDemo.java
@@ -2,25 +2,25 @@ package com.urise.webapp;
 
 public class DeadlockDemo {
 
-    public final static Object one=new Object(), two=new Object();
+    public static void main(String[] args) {
 
-    public static void main(String s[]) {
-
-        Thread t1 = getThread(one,two);
-        Thread t2 = getThread(two,one);
-
-        t1.start();
-        t2.start();
+        Object one = new Object();
+        Object two = new Object();
+        getThread(one, two).start();
+        getThread(two, one).start();
     }
 
-    private static Thread getThread(Object obj1,Object obj2) {
+    private static Thread getThread(Object obj1, Object obj2) {
         return new Thread(() -> {
-            synchronized(obj1) {
+            System.out.println(Thread.currentThread().getName() + " пытается захватить " + obj1);
+            synchronized (obj1) {
+                System.out.println(Thread.currentThread().getName() + " захватил " + obj1);
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                System.out.println(Thread.currentThread().getName() + " пытается захватить " + obj2);
                 synchronized (obj2) {
                     System.out.println("Success!");
                 }
