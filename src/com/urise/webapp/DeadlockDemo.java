a343a4568706678dcb35b59a3410e469f28913a1
Dmitrii Solovev <soloveid87@mail.ru>
2021-05-25 10:20:47 +1000

    done

--- src/com/urise/webapp/DeadlockDemo.java
+++ src/com/urise/webapp/DeadlockDemo.java
@@ -12,15 +12,15 @@ public class DeadlockDemo {
 
     private static Thread getThread(Object obj1, Object obj2) {
         return new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + " пытается захватить " + obj1);
+            System.out.println(Thread.currentThread().getName() + " trying to get " + obj1);
             synchronized (obj1) {
-                System.out.println(Thread.currentThread().getName() + " захватил " + obj1);
+                System.out.println(Thread.currentThread().getName() + " got " + obj1);
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println(Thread.currentThread().getName() + " пытается захватить " + obj2);
+                System.out.println(Thread.currentThread().getName() + " trying to get " + obj2);
                 synchronized (obj2) {
                     System.out.println("Success!");
                 }
