e1dabdad5f9b04bcee362a71224822e2da1114d6
Dmitrii Solovev <soloveid87@mail.ru>
2021-05-26 11:02:51 +1000

    stream

--- src/MainStream.java
+++ src/MainStream.java
@@ -4,26 +4,35 @@ import java.util.stream.Collectors;
 
 public class MainStream {
 
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+        int min = minValue(arr);
+        System.out.println(min);
+
+        List<Integer> list = Arrays.asList(1, 2, 3);
+        List<Integer> result = oddOrEven(list);
+        System.out.println(result);
+    }
+
     public static int minValue(int[] values) {
 
-        String number = Arrays.stream(values).distinct().sorted().mapToObj(String::valueOf).reduce((x, y) -> x + y).get();
-        return Integer.parseInt(number);
+        int number = Arrays.stream(values).distinct().sorted()
+                .reduce((a, b) -> a * 10 + b).getAsInt();
+        return number;
     }
 
     public static List<Integer> oddOrEven(List<Integer> integers) {
 
         int sum = integers.stream().mapToInt(i -> i).sum();
 
-        List<Integer> oddOrEven;
-        if (sum % 2 == 0) {
-            oddOrEven = integers.stream().filter(x -> x % 2 != 0).collect(Collectors.toList());
-        }
-        oddOrEven = integers.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+        List<Integer> oddOrEven = integers.stream().filter(x -> {
+            if (sum % 2 == 0) {
+                return x % 2 != 0;
+            } else
+                return x % 2 == 0;
+        }).collect(Collectors.toList());
 
         return oddOrEven;
-    }
-
-    public static void main(String[] args) {
-
     }
 }
