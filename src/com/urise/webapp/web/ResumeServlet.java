1fd111e8cd1161533bd1a72f125bf92d61a91066
Dmitrii Solovev <soloveid87@mail.ru>
2021-08-04 15:46:41 +1000

    servlet

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -1,7 +1,8 @@
 package com.urise.webapp.web;
 
+import com.urise.webapp.Config;
 import com.urise.webapp.model.Resume;
-import com.urise.webapp.storage.SqlStorage;
+import com.urise.webapp.storage.Storage;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,16 +13,27 @@ import java.io.IOException;
 import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
+
+    Storage storage =  Config.get().getStorage();
+
+//    @Override
+//    public void init(ServletConfig config) throws ServletException {
+//        super.init(config);
+//        storage = Config.get().getStorage();
+//    }
+
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        request.setCharacterEncoding("UTF-8");
-//        response.setCharacterEncoding("UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
 //        response.setHeader("Content-Type", "text/html; charset=UTF-8");
-//        response.setContentType("text/html; charset=UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
 //        String name = request.getParameter("name");
 //        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
 
-        List<Resume> resumeList = new SqlStorage("jdbc:postgresql://localhost:5432/resumes", "postgres", "postgres").getAllSorted();
+        //List<Resume> resumeList = new SqlStorage("jdbc:postgresql://localhost:5432/resumes", "postgres", "postgres").getAllSorted();
+        List<Resume> resumeList = storage.getAllSorted();
         request.setAttribute("resumes", resumeList);
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("resume.jsp");
         requestDispatcher.forward(request, response);
